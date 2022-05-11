7841f0e183640f6836bc17aada7416594f764fa0
Roshan Ghojoghi <roshanghojoghi@gmail.com>
2022-05-11 18:49:19 +0300

    form added

---
+++ index.js
@@ -0,0 +1,6 @@
+const input = document.querySelector('.form__email');
+const iconError = document.querySelector('.form__error-icon-box');
+
+input.addEventListener('invalid', (e) => {
+    iconError.style('visibility', 'visible');
+})
