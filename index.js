eb21167e6ab33b35dc95886fdc02e4e8f53b765a
Roshan Ghojoghi <roshanghojoghi@gmail.com>
2022-05-11 19:39:22 +0300

    responsive image added

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const input = document.querySelector('.form__email');
 const iconError = document.querySelector('.form__error-icon-box');
 
 input.addEventListener('invalid', (e) => {
-    iconError.style('visibility', 'visible');
+    iconError.style.visibility = 'visible';
 })
